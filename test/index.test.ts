8055295881c9cf26978d29f36423bd83382e954e
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2022-07-08 20:20:52 +0800

    remove index testing code, promote it to jest test suite

--- test/index.test.ts
+++ test/index.test.ts
@@ -90,4 +90,61 @@ export class TestSuite {
       'a',
     ])
   }
+
+  @test
+  async 'test object'(): Promise<void> {
+    await this.db.setObjectField(['test', 'test1'], 'foo', 'bar')
+    expect(await this.db.getObjectField('test', 'foo')).toBe('bar')
+    expect(await this.db.getObjectField('test1', 'foo')).toBe('bar')
+
+    await this.db.setObjectField('test', 'bar', 1234)
+    expect(await this.db.getObjectField('test', 'bar')).toBe(1234)
+
+    await this.db.setObject(['test', 'test1'], {
+      abcd: { test: 1234 },
+      hello: 'world',
+    })
+    expect(await this.db.getObjectField('test', 'abcd')).toStrictEqual(
+      await this.db.getObjectField('test1', 'abcd'),
+    )
+
+    expect(await this.db.getObjectKeys('test')).toIncludeAllMembers([
+      'foo',
+      'bar',
+      'abcd',
+      'hello',
+    ])
+    expect(await this.db.getObjectValues('test')).toIncludeAllMembers([
+      { test: 1234 },
+      'world',
+      1234,
+      'bar',
+    ])
+    expect(await this.db.getObject('test', [])).toStrictEqual({
+      abcd: { test: 1234 },
+      bar: 1234,
+      foo: 'bar',
+      hello: 'world',
+    })
+    expect(
+      await this.db.getObject('test', ['foo', 'foo1', 'bar', 'abcd']),
+    ).toStrictEqual({
+      abcd: { test: 1234 },
+      bar: 1234,
+      foo: 'bar',
+      foo1: null,
+    })
+    expect(await this.db.isObjectField('test', 'abcd')).toBeTruthy()
+    await this.db.deleteObjectField('test', 'abcd')
+
+    expect(await this.db.isObjectFields('test', ['abcd', 'foo'])).toBeTruthy()
+    expect(await this.db.incrObjectField('test1', 'count')).toBe(1)
+    expect(await this.db.getObjectField('test1', 'count')).toBe(1)
+    expect(
+      await this.db.incrObjectField(['test1', 'test6'], 'count'),
+    ).toStrictEqual([2, 1])
+    expect(
+      await this.db.getObjectsFields(['test1', 'test6'], ['count']),
+    ).toStrictEqual([{ count: 2 }, { count: 1 }])
+  }
 }
