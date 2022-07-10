80eb6fd9fea364a4e251521c0d6e1553aa41b48f
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2022-07-10 17:56:56 +0800

    use a fast cartesianProduct implementation

--- src/utils.ts
+++ src/utils.ts
@@ -84,9 +84,113 @@ export function convertRedisStyleRangeStringToTypeormCriterion(
   return MoreThanOrEqual(Number(input))
 }
 
-export const cartesianProduct = <T>(...sets: T[][]): T[][] =>
-  sets.reduce<T[][]>(
-    (accSets, set) =>
-      accSets.flatMap((accSet) => set.map((value) => [...accSet, value])),
-    [[]],
+export function validateInput<T>(arrays: T[][]): void {
+  if (!Array.isArray(arrays)) {
+    throw new TypeError('Argument must be an array of arrays')
+  }
+
+  arrays.forEach(validateArray)
+  validateDimensions(arrays)
+  validateCombinations(arrays)
+}
+
+function validateArray<T>(array: T[]): void {
+  if (!Array.isArray(array)) {
+    throw new TypeError(`Argument must be an array: ${array}`)
+  }
+}
+
+// Maximum number of nested `for` loops. In my machine, it's 604 but it is
+// engine-specific so we use a safe number. Above the limit, a max call stack
+// error is thrown by the engine.
+function validateDimensions({ length }: { length: number }): void {
+  if (length >= MAX_DIMENSIONS) {
+    throw new TypeError(
+      `Too many arrays (${length}): please use the 'big-cartesian' library instead of 'fast-cartesian'`,
+    )
+  }
+}
+
+const MAX_DIMENSIONS = 1e2
+
+// Max array size in JavaScript. This is the limit of th e final return value.
+function validateCombinations<T>(arrays: T[][]): void {
+  const size = arrays.reduce((n, array) => n * array.length, 1)
+
+  if (size >= MAX_SIZE) {
+    const sizeStr = Number.isFinite(size) ? ` (${size.toExponential(0)})` : ''
+    throw new TypeError(
+      `Too many combinations${sizeStr}: please use the 'big-cartesian' library instead of 'fast-cartesian'`,
+    )
+  }
+}
+
+// eslint-disable-next-line no-magic-numbers
+const MAX_SIZE = 2 ** 32
+
+export function cartesianProduct<InputArrays extends any[][]>(
+  factors: [...InputArrays],
+): InputArrays extends []
+  ? []
+  : {
+      [index in keyof InputArrays]: InputArrays[index] extends Array<
+        infer InputElement
+      >
+        ? InputElement
+        : never
+    }[]
+
+export function cartesianProduct(arrays: any[]): any[] {
+  validateInput(arrays)
+
+  if (arrays.length === 0) {
+    return []
+  }
+
+  const loopFunc = getLoopFunc(arrays.length)
+  const result: any[] = []
+  loopFunc(arrays, result)
+  return result
+}
+
+function getLoopFunc(length: number): (arrays: any[], results: any[]) => void {
+  const cachedLoopFunc = cache[length]
+
+  if (cachedLoopFunc !== undefined) {
+    return cachedLoopFunc
+  }
+
+  const loopFunc = mGetLoopFunc(length)
+  cache[length] = loopFunc
+  return loopFunc
+}
+
+const cache: {
+  [length: number]: (arrays: any[], results: [any, any][]) => void
+} = {}
+
+// Create a function with `new Function()` that does:
+//   function(arrays, results) {
+//     for (const value0 of arrays[0]) {
+//       for (const value1 of arrays[1]) {
+//         // and so on
+//         results.push([value0, value1])
+//       }
+//     }
+//   }
+function mGetLoopFunc(length: number): (arrays: any[], results: any[]) => void {
+  const indexes = Array.from({ length }, (_, index) => String(index))
+  const start = indexes
+    .map((index) => `for (const value${index} of arrays[${index}]) {`)
+    .join('\n')
+  const middle = indexes.map((index) => `value${index}`).join(', ')
+  const end = '}\n'.repeat(length)
+
+  // eslint-disable-next-line no-new-func
+  // @ts-ignore
+  return new Function(
+    'arrays',
+    'result',
+    `${start}\nresult.push([${middle}])\n${end}`,
   )
+}
